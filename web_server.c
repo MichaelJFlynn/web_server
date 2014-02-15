5191c0fbd448ec3a66fa9ccf00b3eb2c6f842aa1
Jeremy Boissevain <jrb7@williams.edu>
2014-02-15 12:59:30 -0500

    Added Threads

--- web_server.c
+++ web_server.c
@@ -4,6 +4,13 @@
 #include <fcntl.h>
 #include <pthread.h>
 
+typedef struct{
+  int connection;
+  struct sockaddr_in client;
+} thread_arg;
+
+void *handleConnection(void*);
+
 int main(int argc, char ** argv) {
 
   int fd, nread;
@@ -21,11 +28,13 @@ int main(int argc, char ** argv) {
   
   if(bind(sock, (struct sockaddr *) &myaddr, sizeof(myaddr))< 0) {
     printf("error binding socket");
+    close(sock);
     return -1;
   }
   
   if(listen(sock, 15) < 0) {
     printf("error listening to socket");
+    close(sock);
     return -1;
   }
 
@@ -33,16 +42,37 @@ int main(int argc, char ** argv) {
   // todo: spawn new thread to handle connection
   len = sizeof(cliaddr);
   int i = 0;
+  pthread_t tid;
   while(i < 5) {
     int connection = accept(sock, (struct sockaddr *) &cliaddr, &len);
-    printf("Connection from %s, port %d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
-
-    write(connection, "Hello I am Mike\n", sizeof("Hello I am Mike\n"));
-    close(connection);
+    if(connection == -1){
+      printf("Connection failed");
+    }
+    struct thread_arg arg = (thread_arg) *malloc(sizeof(thread_arg));
+    arg.connection = connection;
+    arg.client = cliaddr;
+    int thread = pthread_create(&tid, NULL, handleConnection, &arg);
     i++;
   } 
   close(sock);
   
 
   return 0;
+}
+
+
+void * handleConnection(void * arg) {
+  struct thread_arg args = (thread_arg) *arg;
+  int connection = args.connection;
+  struct sockaddr_in cliaddr = args.client;
+
+
+
+  printf("Connection from %s, port %d\n", inet_ntoa(cliaddr.sin_addr), ntohs(cliaddr.sin_port));
+  write(connection, "Hello I am Mike\n", sizeof("Hello I am Mike\n"));
+  close(connection);
+
+
+  free(arg);
+  return(NULL);
 }
